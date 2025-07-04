01e1d0870fd512a35f26073c7a8bf4af463bf551
minhvu278 <dominhvu278@gmail.com>
2025-07-04 17:33:16 +0700

    update port 6k

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT || 3000);
+  const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 6000;
+  await app.listen(port);
 }
 bootstrap();
